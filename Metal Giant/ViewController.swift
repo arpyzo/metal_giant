1479fb8d9ff8b16669d25829bcdb4fc289d3959d
Robert Pyzalski <robert.pyzalski@shiftboard.com>
2018-08-19 22:36:39 -0700

    Initial Commit

---
+++ Metal Giant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Metal Giant
+//
+//  Created by Robert Pyzalski on 8/19/18.
+//  Copyright © 2018 Robert Pyzalski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
