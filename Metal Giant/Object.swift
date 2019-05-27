40bdce83809e5e70d891e9c8b49a6e045eb22fec
Robert Pyzalski <robert.pyzalski@shiftboard.com>
2019-05-27 10:25:05 -0700

    removing rendering code from object

--- Metal Giant/Object.swift
+++ Metal Giant/Object.swift
@@ -6,23 +6,10 @@ class Object {
     let metalDevice: MTLDevice
     var vertexCount: Int
     var vertexBuffer: MTLBuffer
-    var bufferProvider: BufferProvider
     
-    var positionX: Float = 0.0
-    var positionY: Float = 0.0
-    var positionZ: Float = 0.0
-    
-    var rotationX: Float = 0.0
-    var rotationY: Float = 0.0
-    var rotationZ: Float = 0.0
-    var scale: Float     = 1.0
-    
-    var time:CFTimeInterval = 0.0
-    
-    let light = Light(color: (1.0,1.0,1.0), ambientIntensity: 0.1, direction: (0.0, 0.0, 1.0), diffuseIntensity: 0.8, shininess: 10, specularIntensity: 2)
+    //var time:CFTimeInterval = 0.0
     
     var texture: MTLTexture
-    lazy var samplerState: MTLSamplerState? = Object.defaultSampler(device: self.metalDevice)
     
     init(_ metalDevice: MTLDevice, _ textureLoader: MTKTextureLoader) {
         // Front
@@ -85,93 +72,11 @@ class Object {
         vertexCount = vertices.count
         
         self.texture = texture
-        
-        //self.bufferProvider = BufferProvider(device: device, inflightBuffersCount: 3, sizeOfUniformsBuffer: MemoryLayout<Float>.size * Matrix4.numberOfElements() * 2)
-        let sizeOfUniformsBuffer = MemoryLayout<Float>.size * float4x4.numberOfElements() * 2 + Light.size()
-        self.bufferProvider = BufferProvider(metalDevice: metalDevice, inflightBuffersCount: 3, sizeOfUniformsBuffer: sizeOfUniformsBuffer)
-
     }
     
-    func render(commandQueue: MTLCommandQueue, pipelineState: MTLRenderPipelineState, drawable: CAMetalDrawable, parentModelViewMatrix: float4x4, projectionMatrix: float4x4, clearColor: MTLClearColor?) {
-        
-        _ = bufferProvider.avaliableResourcesSemaphore.wait(timeout: DispatchTime.distantFuture)
-        
-        let renderPassDescriptor = MTLRenderPassDescriptor()
-        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
-        renderPassDescriptor.colorAttachments[0].loadAction = .clear
-        // GREEN
-        //renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
-        // BLACK
-        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
-
-        renderPassDescriptor.colorAttachments[0].storeAction = .store
-        
-        let commandBuffer = commandQueue.makeCommandBuffer()
-        commandBuffer?.addCompletedHandler { (_) in
-            self.bufferProvider.avaliableResourcesSemaphore.signal()
-        }
-        
-        let renderEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
-        renderEncoder?.setCullMode(MTLCullMode.front)
-        renderEncoder?.setRenderPipelineState(pipelineState)
-        renderEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-        
-        
-        renderEncoder?.setFragmentTexture(texture, index: 0)
-        if let samplerState = samplerState{
-            renderEncoder?.setFragmentSamplerState(samplerState, index: 0)
-        }
-
-        var nodeModelMatrix = self.modelMatrix()
-        nodeModelMatrix.multiplyLeft(matrix: parentModelViewMatrix)
-
-        //let uniformBuffer = device.makeBuffer(length: MemoryLayout<Float>.size * Matrix4.numberOfElements() * 2, options: [])
-
-        //let bufferPointer = uniformBuffer?.contents()
-
-        //memcpy(bufferPointer, nodeModelMatrix.raw(), MemoryLayout<Float>.size * Matrix4.numberOfElements())
-        //memcpy(bufferPointer?.advanced(by: MemoryLayout<Float>.size * Matrix4.numberOfElements()), projectionMatrix.raw(), MemoryLayout<Float>.size * Matrix4.numberOfElements())
-        
-        let uniformBuffer = bufferProvider.nextUniformsBuffer(projectionMatrix: projectionMatrix, modelViewMatrix: nodeModelMatrix, light: light)
-
-        renderEncoder?.setVertexBuffer(uniformBuffer, offset: 0, index: 1)
-        renderEncoder?.setFragmentBuffer(uniformBuffer, offset: 0, index: 1)
-
-        
-        renderEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertexCount,
-                                     instanceCount: vertexCount/3)
-        renderEncoder?.endEncoding()
-        
-        commandBuffer?.present(drawable)
-        commandBuffer?.commit()
-    }
-    
-    func modelMatrix() -> float4x4 {
-        var matrix = float4x4()
-        matrix.translate(x: positionX, y: positionY, z: positionZ)
-        matrix.rotateAroundX(x: rotationX, y: rotationY, z: rotationZ)
-        matrix.scale(x: scale, y: scale, z: scale)
-        return matrix
-    }
-    
-    func updateWithDelta(delta: CFTimeInterval){
+    /*func updateWithDelta(delta: CFTimeInterval){
         time += delta
-    }
-    
-    class func defaultSampler(device: MTLDevice) -> MTLSamplerState {
-        let sampler = MTLSamplerDescriptor()
-        sampler.minFilter             = MTLSamplerMinMagFilter.nearest
-        sampler.magFilter             = MTLSamplerMinMagFilter.nearest
-        sampler.mipFilter             = MTLSamplerMipFilter.nearest
-        sampler.maxAnisotropy         = 1
-        sampler.sAddressMode          = MTLSamplerAddressMode.clampToEdge
-        sampler.tAddressMode          = MTLSamplerAddressMode.clampToEdge
-        sampler.rAddressMode          = MTLSamplerAddressMode.clampToEdge
-        sampler.normalizedCoordinates = true
-        sampler.lodMinClamp           = 0
-        sampler.lodMaxClamp           = .greatestFiniteMagnitude
-        return device.makeSamplerState(descriptor: sampler)!
-    }
+    }*/
     
     /*func updateWithDelta(delta: CFTimeInterval) {
         super.updateWithDelta(delta: delta)
--- Metal Giant/Renderer.swift
+++ Metal Giant/Renderer.swift
@@ -8,6 +8,20 @@ class Renderer: NSObject, MTKViewDelegate {
     var commandQueue: MTLCommandQueue!
     var textureLoader: MTKTextureLoader!
     
+    var bufferProvider: BufferProvider
+    let light = Light(color: (1.0,1.0,1.0), ambientIntensity: 0.1, direction: (0.0, 0.0, 1.0), diffuseIntensity: 0.8, shininess: 10, specularIntensity: 2)
+    
+    var positionX: Float = 0.0
+    var positionY: Float = 0.0
+    var positionZ: Float = 0.0
+    
+    var rotationX: Float = 0.0
+    var rotationY: Float = 0.0
+    var rotationZ: Float = 0.0
+    var scale: Float     = 1.0
+    
+    lazy var samplerState: MTLSamplerState? = Renderer.defaultSampler(device: self.metalDevice)
+    
     init(_ metalDevice: MTLDevice) {
         self.metalDevice = metalDevice
         
@@ -22,6 +36,9 @@ class Renderer: NSObject, MTKViewDelegate {
         pipelineState = try! metalDevice.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
         
         commandQueue = metalDevice.makeCommandQueue()
+        
+        let sizeOfUniformsBuffer = MemoryLayout<Float>.size * float4x4.numberOfElements() * 2 + Light.size()
+        self.bufferProvider = BufferProvider(metalDevice: metalDevice, inflightBuffersCount: 3, sizeOfUniformsBuffer: sizeOfUniformsBuffer)
     }
     
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
@@ -38,6 +55,70 @@ class Renderer: NSObject, MTKViewDelegate {
         
         guard let drawable = view.currentDrawable else { return }
         
-        scene.objectToDraw.render(commandQueue: commandQueue, pipelineState: pipelineState, drawable: drawable, parentModelViewMatrix: scene.worldModelMatrix, projectionMatrix: scene.projectionMatrix, clearColor: nil)
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .clear
+        // GREEN
+        //renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
+        // BLACK
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        
+        renderPassDescriptor.colorAttachments[0].storeAction = .store
+        
+        _ = bufferProvider.avaliableResourcesSemaphore.wait(timeout: DispatchTime.distantFuture)
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        commandBuffer?.addCompletedHandler { (_) in
+            self.bufferProvider.avaliableResourcesSemaphore.signal()
+        }
+        
+        let renderEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        renderEncoder?.setCullMode(MTLCullMode.front)
+        renderEncoder?.setRenderPipelineState(pipelineState)
+        renderEncoder?.setVertexBuffer(scene.objectToDraw.vertexBuffer, offset: 0, index: 0)
+        
+        renderEncoder?.setFragmentTexture(scene.objectToDraw.texture, index: 0)
+        if let samplerState = samplerState{
+            renderEncoder?.setFragmentSamplerState(samplerState, index: 0)
+        }
+        
+        var nodeModelMatrix = self.modelMatrix()
+        nodeModelMatrix.multiplyLeft(matrix: scene.worldModelMatrix)
+
+        let uniformBuffer = bufferProvider.nextUniformsBuffer(projectionMatrix: scene.projectionMatrix, modelViewMatrix: nodeModelMatrix, light: light)
+        
+        renderEncoder?.setVertexBuffer(uniformBuffer, offset: 0, index: 1)
+        renderEncoder?.setFragmentBuffer(uniformBuffer, offset: 0, index: 1)
+        
+        
+        renderEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: scene.objectToDraw.vertexCount,
+                                      instanceCount: scene.objectToDraw.vertexCount/3)
+        renderEncoder?.endEncoding()
+        
+        commandBuffer?.present(drawable)
+        commandBuffer?.commit()
+    }
+    
+    func modelMatrix() -> float4x4 {
+        var matrix = float4x4()
+        matrix.translate(x: positionX, y: positionY, z: positionZ)
+        matrix.rotateAroundX(x: rotationX, y: rotationY, z: rotationZ)
+        matrix.scale(x: scale, y: scale, z: scale)
+        return matrix
+    }
+    
+    class func defaultSampler(device: MTLDevice) -> MTLSamplerState {
+        let sampler = MTLSamplerDescriptor()
+        sampler.minFilter             = MTLSamplerMinMagFilter.nearest
+        sampler.magFilter             = MTLSamplerMinMagFilter.nearest
+        sampler.mipFilter             = MTLSamplerMipFilter.nearest
+        sampler.maxAnisotropy         = 1
+        sampler.sAddressMode          = MTLSamplerAddressMode.clampToEdge
+        sampler.tAddressMode          = MTLSamplerAddressMode.clampToEdge
+        sampler.rAddressMode          = MTLSamplerAddressMode.clampToEdge
+        sampler.normalizedCoordinates = true
+        sampler.lodMinClamp           = 0
+        sampler.lodMaxClamp           = .greatestFiniteMagnitude
+        return device.makeSamplerState(descriptor: sampler)!
     }
 }
